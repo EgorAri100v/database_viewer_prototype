1b0db126a05bc5d1aca21258e5ff70765ab3200a
Egor_Aristov <egorari100v@gmail.com>
2025-07-14 20:50:08 +0300

    initial commit

---
+++ test_db_3/test_db_3/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace test_db_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_display_all = new System.Windows.Forms.Button();
+            this.btn_display_non_explicit = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(596, 461);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_display_all
+            // 
+            this.btn_display_all.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn_display_all.Location = new System.Drawing.Point(12, 35);
+            this.btn_display_all.Name = "btn_display_all";
+            this.btn_display_all.Size = new System.Drawing.Size(148, 31);
+            this.btn_display_all.TabIndex = 1;
+            this.btn_display_all.Text = "Display all tracks";
+            this.btn_display_all.UseVisualStyleBackColor = true;
+            this.btn_display_all.Click += new System.EventHandler(this.btn_display_all_Click);
+            // 
+            // btn_display_non_explicit
+            // 
+            this.btn_display_non_explicit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn_display_non_explicit.Location = new System.Drawing.Point(175, 35);
+            this.btn_display_non_explicit.Name = "btn_display_non_explicit";
+            this.btn_display_non_explicit.Size = new System.Drawing.Size(209, 31);
+            this.btn_display_non_explicit.TabIndex = 2;
+            this.btn_display_non_explicit.Text = "Display all not-explicit tracks";
+            this.btn_display_non_explicit.UseVisualStyleBackColor = true;
+            this.btn_display_non_explicit.Click += new System.EventHandler(this.btn_display_non_explicit_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(614, 79);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(344, 461);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(614, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(606, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Type artist\'s name and press Enter to display all his songs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 552);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btn_display_non_explicit);
+            this.Controls.Add(this.btn_display_all);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_display_all;
+        private System.Windows.Forms.Button btn_display_non_explicit;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ test_db_3/test_db_3/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace test_db_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_display_all_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=test_db;User Id=postgres;Password=123321;");
+            connection.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT m.songname, a.artist_name, m.duration_seconds, m.explicit FROM music_tracks AS m JOIN artists AS a ON m.artist_id = a.artist_id";
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = dt;
+            }
+            cmd.Dispose();
+            connection.Close();
+        }
+
+        private void btn_display_non_explicit_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=test_db;User Id=postgres;Password=123321;");
+            connection.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT m.songname, a.artist_name, m.duration_seconds FROM music_tracks AS m JOIN artists AS a ON m.artist_id = a.artist_id WHERE m.explicit = FALSE";
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = dt;
+            }
+            cmd.Dispose();
+            connection.Close();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=test_db;User Id=postgres;Password=123321;");
+                connection.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT m.songname FROM music_tracks AS m JOIN artists AS a ON m.artist_id = a.artist_id WHERE a.artist_name = '" + textBox1.Text + "'";
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(dr);
+                    dataGridView2.DataSource = dt;
+                }
+                cmd.Dispose();
+                connection.Close();
+                textBox1.Text = "";
+            }
+        }
+    }
+}
